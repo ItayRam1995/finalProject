9fd35e8d6d0fba762798591d3aeb4a1a52a61842
Itay Ram <itay.ram01@gmail.com>
2025-05-23 22:58:59 +0300

    no message

--- registration/user/my_orders.php
+++ registration/user/my_orders.php
@@ -11,18 +11,6 @@ $dbname = "itayrm_dogs_boarding_house";
 $conn = new mysqli($servername, $username, $password, $dbname);
 $conn->set_charset("utf8");
 
-/*
-$host="127.0.0.1";
-$port=3306;
-$socket="";
-$user="root";
-$password="";
-$dbname="itayrm_dogs_boarding_house";
-
-$conn = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
-*/
-
 // בדיקת חיבור
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
